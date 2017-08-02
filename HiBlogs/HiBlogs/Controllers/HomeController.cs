af56b2d9fbeb0c57cddb1bc4da0091eeccfe0b28
benny <zhaopeiym@163.com>
2017-08-02 22:11:58 +0800

    启动！！！

---
+++ HiBlogs/HiBlogs/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HiBlogs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
